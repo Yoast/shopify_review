3b5af24a51e895de63fe721491efbfcaf19c01eb
honglei <honglei.zhang@yoast.com>
2022-12-19 20:54:33 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 19 20:11:43 2022
+
+@author: Honglei
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='scrapy shopify review',
+    version='0.1.0',
+    description='Setting up shopify review',
+    author='honglei/yoast',
+    author_email='honglei.zhang@yoast.com',
+    url='https://blog.godatadriven.com/setup-py',
+    packages=find_packages(include=['shopify_review']),
+    install_requires=[
+        'scrapy'
+    ],
+    extras_require={'plotting': ['matplotlib>=2.2.0', 'jupyter']}
+)
